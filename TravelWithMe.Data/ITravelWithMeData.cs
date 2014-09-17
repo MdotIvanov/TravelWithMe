a4073d8ef6dc1e26aa4df69e346d306d62931dac
Simeon Cholakov <sime679@gmail.com>
2014-09-18 00:47:44 +0300

    Adding the new project

---
+++ TravelWithMe.Data/ITravelWithMeData.cs
@@ -0,0 +1,25 @@
+﻿namespace TravelWithMe.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using TravelWithMe.Data.Repositories;
+    using TravelWithMe.Models;
+
+    public interface ITravelWithMeData
+    {
+        IRepository<TravelWithMeUser> Users { get; }
+
+        IRepository<Travel> Travels { get; }
+
+        IRepository<City> Cities { get; }
+
+        int SaveChanges();
+
+
+
+
+    }
+}
---
+++ TravelWithMe.Models/Travel.cs
@@ -0,0 +1,28 @@
+﻿namespace TravelWithMe.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Travel
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public virtual TravelWithMeUser User { get; set; }
+
+        public DateTime TravelDate { get; set; }
+
+        public int StartLocationId { get; set; }
+
+        public virtual City StartLocation { get; set; }
+
+        public int EndLocatonId { get; set; }
+
+        public virtual City EndLocation { get; set; }
+
+        [MinLength(3)]
+        [MaxLength(20)]
+        public string PhoneNumber { get; set; }
+    }
+}
